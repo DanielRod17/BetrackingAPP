acfd05668594a74ef7abcaa45a7c26a54c684217
DanielRod17 <32774292+DanielRod17@users.noreply.github.com>
2019-07-15 22:07:23 -0600

    Inputs Time In/Out

---
+++ BetrackingAPP/BetrackingAPP/Models/NewTimeOut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetrackingAPP.Models
+{
+    public class NewTimeOutCard
+    {
+        public string Day { get; set; }
+        public int Numero { get; set; }
+        public decimal Valor { get; set; }
+        public string Nota { get; set; }
+        public int DisplayInputs { get; set; }
+    }
+}
